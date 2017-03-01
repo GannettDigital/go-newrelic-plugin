ac052c4d656212a394b070f60de07b2e6d5f356c
Ryan Grothouse <rgrothouse@gmail.com>
2017-03-01 11:47:05 -0500

    PAAS-4907: restructure and stub out methods

---
+++ types.go
@@ -0,0 +1,40 @@
+package goNewRelicCollector
+
+// a collector does some work to gather stats and
+// returns a set of key => values that indicate metric type (key) and metric value
+// its highly recommended that you namespace your metrics like <type>.<level1>.<leve2> etc
+// ie: the haproxy Collector might return data as follows
+/*
+  map[string]string{
+      "haproxy.beresp.500": "4",
+      "haproxy.beresp.200": "100",
+  }
+
+*/
+type Collector func(config Config, stats chan<- map[string]string)
+
+// TODO:
+// source from config.yaml
+
+/*
+
+Config example:
+
+---
+haproxy:
+  enabled: true
+  check_interval_ms: 100
+nginx:
+  enabled: true
+  check_interval_ms: 1000
+  listen_port: 80
+  status_uri: /foo
+  status_page: /derp
+tags:
+    region: us-east1
+    farm: derp-farm
+
+*/
+
+type Config struct {
+}
