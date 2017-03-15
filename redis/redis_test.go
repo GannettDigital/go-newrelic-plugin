f460e9c5e2589d69320fa00ef15592a162ec8acd
barberta <tbarber@gannett.com>
2017-03-15 18:00:39 -0400

    PAAS-5130 - update testing

--- redis/redis_test.go
+++ redis/redis_test.go
@@ -149,3 +149,115 @@ func TestValidateConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestParseRawData(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	var tests = []struct {
+		InputData       string
+		ExpectedRes     map[string]string
+		TestDescription string
+	}{
+		{
+			InputData: "key1:value1\r\nkey2:value2\r\nkey3:value3\r\n",
+			ExpectedRes: map[string]string{
+				"key1": "value1",
+				"key2": "value2",
+				"key3": "value3",
+			},
+			TestDescription: "Should successfully parse and format output string into map",
+		},
+		{
+			InputData:       "key1:value1\rkey2:value2\rkey3:value3\r",
+			ExpectedRes:     map[string]string{},
+			TestDescription: "Should return empty map if data is not formatted properly",
+		},
+	}
+
+	for _, test := range tests {
+		g.Describe("parseRawData()", func() {
+			g.It(test.TestDescription, func() {
+				res := parseRawData(test.InputData)
+				g.Assert(res).Equal(test.ExpectedRes)
+			})
+		})
+	}
+}
+
+func TestToInt(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	var tests = []struct {
+		InputLog        *logrus.Logger
+		InputValue      string
+		ExpectedRes     int
+		TestDescription string
+	}{
+		{
+			InputLog:        logrus.New(),
+			InputValue:      "3",
+			ExpectedRes:     3,
+			TestDescription: "Should successfully parse to int when given valid int value",
+		},
+		{
+			InputLog:        logrus.New(),
+			InputValue:      "puppies",
+			ExpectedRes:     0,
+			TestDescription: "Should return 0 when given no int input",
+		},
+		{
+			InputLog:        logrus.New(),
+			InputValue:      "",
+			ExpectedRes:     0,
+			TestDescription: "Should return 0 when given blank input",
+		},
+	}
+
+	for _, test := range tests {
+		g.Describe("toInt()", func() {
+			g.It(test.TestDescription, func() {
+				res := toInt(test.InputLog, test.InputValue)
+				g.Assert(res).Equal(test.ExpectedRes)
+			})
+		})
+	}
+}
+
+func TestToFloat(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	var tests = []struct {
+		InputLog        *logrus.Logger
+		InputValue      string
+		ExpectedRes     float64
+		TestDescription string
+	}{
+		{
+			InputLog:        logrus.New(),
+			InputValue:      "3.14",
+			ExpectedRes:     3.14,
+			TestDescription: "Should successfully parse to float64 when given valid float value",
+		},
+		{
+			InputLog:        logrus.New(),
+			InputValue:      "puppies",
+			ExpectedRes:     0,
+			TestDescription: "Should return 0 when given no float input",
+		},
+		{
+			InputLog:        logrus.New(),
+			InputValue:      "",
+			ExpectedRes:     0,
+			TestDescription: "Should return 0 when given blank input",
+		},
+	}
+
+	for _, test := range tests {
+		g.Describe("toFloat()", func() {
+			g.It(test.TestDescription, func() {
+				res := toFloat(test.InputLog, test.InputValue)
+				g.Assert(res).Equal(test.ExpectedRes)
+			})
+		})
+	}
+}
