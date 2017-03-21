3bd73be218dce28a4d0155988cfbd8834ff03b6e
Seth Dozier <soylentseth@users.noreply.github.com>
2017-03-21 08:32:49 -0400

    PAAS-4877-added_cmd

---
+++ cmd/haproxy.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/GannettDigital/go-newrelic-plugin/haproxy"
+	status "github.com/GannettDigital/goStateModule"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(haproxyCmd)
+}
+
+var haproxyCmd = &cobra.Command{
+	Use:   "haproxy",
+	Short: "execute an haproxy collection",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info("haproxy collection")
+		haproxy.Run(log, prettyPrint, status.GetInfo().Version)
+	},
+}
