f22d277d617dedc1fbb2bc5564440a79b70c27c1
Denecke <edenecke@gannett.com>
2017-04-07 17:35:25 -0400

    PAAS-5120 added changes based on paas-kraken 2.2.0

--- kraken/kraken.go
+++ kraken/kraken.go
@@ -1,20 +1,35 @@
-package skel
+package kraken
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
+	"github.com/GannettDigital/paas-api-utils/utilsHTTP"
 	"github.com/Sirupsen/logrus"
 )
 
-const NAME string = "skel"
-const PROVIDER string = "skel" //we might want to make this an env tied to nginx version or app name maybe...
-const PROTOCOL_VERSION string = "1"
+var runner utilsHTTP.HTTPRunner
 
-//SkelConfig is the keeper of the config
-type SkelConfig struct {
-	SkelHost string
+// NAME - name of plugin
+const NAME string = "kraken"
+
+// PROVIDER -
+const PROVIDER string = "kraken" //we might want to make this an env tied to kraken version or app name maybe...
+
+// ProtocolVersion -
+const ProtocolVersion string = "1"
+
+//KrakenConfig is the keeper of the config
+type Config struct {
+	KrakenListenPort string
+	KrakenStatusURI  string
+	KrakenHost       string
 }
 
 // InventoryData is the data type for inventory data produced by a plugin data
@@ -64,39 +79,37 @@ func OutputJSON(data interface{}, pretty bool) error {
 	return nil
 }
 
+func init() {
+	runner = utilsHTTP.HTTPRunnerImpl{}
+}
+
 func Run(log *logrus.Logger, prettyPrint bool, version string) {
 
 	// Initialize the output structure
 	var data = PluginData{
 		Name:            NAME,
-		ProtocolVersion: PROTOCOL_VERSION,
+		ProtocolVersion: ProtocolVersion,
 		PluginVersion:   version,
 		Inventory:       make(map[string]InventoryData),
 		Metrics:         make([]MetricData, 0),
 		Events:          make([]EventData, 0),
 	}
 
-	var config = SkelConfig{
-		SkelHost: os.Getenv("KEY"),
+	var krakenConf = Config{
+		KrakenListenPort: os.Getenv("KRAKEN_PORT"),
+		KrakenHost:       os.Getenv("KRAKEN_HOST"),
+		KrakenStatusURI:  os.Getenv("KRAKEN_STATUSURI"),
 	}
-	validateConfig(log, config)
+	validateConfig(log, krakenConf)
 
-	var metric = getMetric(log, config)
+	var metric = scrapeStatus(log, getKrakenStatus(log, krakenConf))
 
 	data.Metrics = append(data.Metrics, metric)
 	fatalIfErr(log, OutputJSON(data, prettyPrint))
 }
 
-func getMetric(log *logrus.Logger, config SkelConfig) map[string]interface{} {
-	return map[string]interface{}{
-		"event_type": "LoadBalancerSample",
-		"provider":   PROVIDER,
-		"skel.stat":  1,
-	}
-}
-
-func validateConfig(log *logrus.Logger, config SkelConfig) {
-	if config.SkelHost == "" {
+func validateConfig(log *logrus.Logger, krakenConf Config) {
+	if krakenConf.KrakenHost == "" || krakenConf.KrakenListenPort == "" || krakenConf.KrakenStatusURI == "" {
 		log.Fatal("Config Yaml is missing values. Please check the config to continue")
 	}
 }
@@ -105,4 +118,127 @@ func fatalIfErr(log *logrus.Logger, err error) {
 	if err != nil {
 		log.WithError(err).Fatal("can't continue")
 	}
+}
+
+func getKrakenStatus(log *logrus.Logger, config Config) string {
+	krakenStatus := fmt.Sprintf("%v:%v/%v", config.KrakenHost, config.KrakenListenPort, config.KrakenStatusURI)
+	httpReq, err := http.NewRequest("GET", krakenStatus, bytes.NewBuffer([]byte("")))
+	// http.NewRequest error
+	fatalIfErr(log, err)
+	code, data, err := runner.CallAPI(log, nil, httpReq, &http.Client{})
+	if err != nil || code != 200 {
+		log.WithFields(logrus.Fields{
+			"code":                   code,
+			"data":                   string(data),
+			"httpReq":                httpReq,
+			"config.KrakenStatusPage": config.KrakenHost,
+			"config.KrakenListenPort": config.KrakenListenPort,
+			"config.KrakenStatusURI":  config.KrakenStatusURI,
+			"error":                  err,
+		}).Fatal("Encountered error calling CallAPI")
+		return ""
+	}
+
+	return string(data)
+}
+
+func scrapeStatus(log *logrus.Logger, status string) map[string]interface{} {
+
+	multi = regexp.MustCompile(`Version: (\s)`).FindString(status)
+	contents = strings.Fields(multi)
+	krakenVersion := contents[0]
+
+	multi = regexp.MustCompile(`Customer: (\s)`).FindString(status)
+	contents = strings.Fields(multi)
+	krakenCustomer := contents[0]
+
+	multi = regexp.MustCompile(`Project: (\s)`).FindString(status)
+	contents = strings.Fields(multi)
+	krakenProject := contents[0]
+
+	multi = regexp.MustCompile(`State: (\s)`).FindString(status)
+	contents = strings.Fields(multi)
+	krakenState := contents[0]
+
+	multi := regexp.MustCompile(`Samples count: (\d+), (\d+(\.\d+)?). failures`).FindString(status)
+	contents := strings.Fields(multi)
+	sample_count := contents[0]
+	sample_failure := contents[1]
+
+	multi = regexp.MustCompile(`Average times: total (\d+(\.\d+)?), latency (\d+(\.\d+)?), connect (\d+(\.\d+)?)`).FindString(status)
+	contents = strings.Fields(multi)
+	avg_resp_time := contents[0]
+	avg_latency := contents[1]
+	avg_conn_time := contents[2]
+
+	multi = regexp.MustCompile(`Percentile  50.0.: (\d+(\.\d+)?)`).FindString(status)
+	contents = strings.Fields(multi)
+	percentiles_50 := contents[0]
+
+	multi = regexp.MustCompile(`Percentile  90.0.: (\d+(\.\d+)?)`).FindString(status)
+	contents = strings.Fields(multi)
+	percentiles_90 := contents[0]
+
+	multi = regexp.MustCompile(`Percentile  95.0.: (\d+(\.\d+)?)`).FindString(status)
+	contents = strings.Fields(multi)
+	percentiles_95 := contents[0]
+
+	multi = regexp.MustCompile(`Percentile  99.0.: (\d+(\.\d+)?)`).FindString(status)
+	contents = strings.Fields(multi)
+	percentiles_99 := contents[0]
+
+	multi = regexp.MustCompile(`Percentile  100.0.: (\d+(\.\d+)?)`).FindString(status)
+	contents = strings.Fields(multi)
+	percentiles_100 := contents[0]
+
+	log.WithFields(logrus.Fields{
+		"kraken_version":  krakenVersion,
+		"kraken_customer":  krakenCustomer,
+		"kraken_project":  krakenProject,
+		"kraken_state":  krakenState,
+		"avg_resp_time":  avg_resp_time,
+		"avg_latency":  avg_latency,
+		"avg_conn_time":  avg_conn_time,
+		"sample_count": sample_count,
+		"sample_failure": sample_failure,
+		"avg_rt":  avg_rt,
+		"duration":  duration,
+	}).Debugf("Scraped KRAKEN values")
+	return map[string]interface{}{
+		"event_type":               "KrakenSample",
+		"provider":                 PROVIDER,
+		"kraken.version":						krakenVersion
+		"kraken.customer":			   	krakenCustomer
+		"kraken.project":						krakenProject
+		"kraken.state":				   		krakenState
+		"kraken.kpi.avg_resp_time": toInt(log, avg_resp_time),
+		"kraken.kpi.avg_latency":   toInt(log, avg_latency),
+		"kraken.kpi.avg_conn_time": toInt(log, avg_conn_time),
+		"kraken.kpi.percentiles.50":   toInt(log, percentiles_50),
+		"kraken.kpi.percentiles.90":   toInt(log, percentiles_90),
+		"kraken.kpi.percentiles.95":   toInt(log, percentiles_95),
+		"kraken.kpi.percentiles.99":   toInt(log, percentiles_99),
+		"kraken.kpi.percentiles.100":   toInt(log, percentiles_100),
+		"kraken.sample_count":      toInt(log, sample_count),
+		"kraken.sample_failure":    toInt(log, sample_failure),
+		"kraken.avg_rt":            toInt(log, avg_rt),
+		"kraken.duration":          toInt(log, duration),
+	}
+}
+
+func toInt(log *logrus.Logger, value string) int {
+	if value == "" {
+		return 0
+	}
+	valueInt, err := strconv.Atoi(value)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"valueInt": valueInt,
+			"error":    err,
+		}).Debug("Error converting value to int")
+
+		return 0
+	}
+
+	return valueInt
 }
---
+++ kraken/kraken_test.go
@@ -0,0 +1,128 @@
+package nginx
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	fake "github.com/GannettDigital/paas-api-utils/utilsHTTP/fake"
+	"github.com/Sirupsen/logrus"
+	"github.com/franela/goblin"
+)
+
+var fakeConfig Config
+
+func init() {
+	fakeConfig = Config{
+		NginxListenPort: "8140",
+		NginxStatusURI:  "nginx_status",
+		NginxHost:       "http://localhost",
+	}
+}
+
+func TestGetNginxStatus(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	var tests = []struct {
+		HTTPRunner      fake.HTTPResult
+		TestDescription string
+	}{
+		{
+			HTTPRunner: fake.HTTPResult{
+				ResultsList: []fake.Result{
+					{
+						Method: "GET",
+						URI:    "/nginx_status",
+						Code:   200,
+						Data:   []byte("Active connections: 2 \nserver accepts handled requests\n 29 29 31 \nReading: 0 Writing: 1 Waiting: 1 "),
+						Err:    nil,
+					},
+				},
+			},
+			TestDescription: "Successfully GET Nginx status page",
+		},
+	}
+
+	for _, test := range tests {
+		g.Describe("getNginxStatus()", func() {
+			g.It(test.TestDescription, func() {
+				runner = test.HTTPRunner
+				result := getNginxStatus(logrus.New(), fakeConfig)
+				g.Assert(reflect.DeepEqual(result, string(test.HTTPRunner.ResultsList[0].Data))).Equal(true)
+			})
+		})
+	}
+}
+
+func TestScrapeStatus(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	result := map[string]interface{}{
+		"event_type":            "LoadBalancerSample",
+		"provider":              "nginx",
+		"nginx.net.connections": 2,
+		"nginx.net.accepts":     29,
+		"nginx.net.handled":     29,
+		"nginx.net.requests":    31,
+		"nginx.net.writing":     1,
+		"nginx.net.waiting":     1,
+		"nginx.net.reading":     0,
+	}
+
+	var tests = []struct {
+		Data            string
+		ExpectedResult  map[string]interface{}
+		TestDescription string
+	}{
+		{
+			Data:            "Active connections: 2 \nserver accepts handled requests\n 29 29 31 \nReading: 0 Writing: 1 Waiting: 1 ",
+			ExpectedResult:  result,
+			TestDescription: "Successfully scrape given status page",
+		},
+	}
+
+	for _, test := range tests {
+		g.Describe("scrapeStatus()", func() {
+			g.It(test.TestDescription, func() {
+				result := scrapeStatus(logrus.New(), test.Data)
+				fmt.Println(result)
+				g.Assert(reflect.DeepEqual(result, test.ExpectedResult)).Equal(true)
+			})
+		})
+	}
+}
+
+func TestToInt(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	var tests = []struct {
+		Value           string
+		ExpectedResult  int
+		TestDescription string
+	}{
+		{
+			Value:           "234567",
+			ExpectedResult:  234567,
+			TestDescription: "Should return int 234567 of string",
+		},
+		{
+			Value:           "",
+			ExpectedResult:  0,
+			TestDescription: "Should return 0 if empty string",
+		},
+		{
+			Value:           "xyz",
+			ExpectedResult:  0,
+			TestDescription: "Should return 0 if error converting to int",
+		},
+	}
+
+	for _, test := range tests {
+		g.Describe("toInt()", func() {
+			g.It(test.TestDescription, func() {
+				result := toInt(logrus.New(), test.Value)
+				g.Assert(reflect.DeepEqual(result, test.ExpectedResult)).Equal(true)
+			})
+		})
+	}
+}
