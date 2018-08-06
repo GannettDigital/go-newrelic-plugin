de7f0b0b851698f367d90d9169f0ea2957a46b39
jstorer <jeremyjstorer@gmail.com>
2018-08-06 15:12:35 -0400

    proper output

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -3,21 +3,22 @@ package datastore
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"time"
+	"strconv"
 
+	"cloud.google.com/go/datastore"
+	"github.com/Sirupsen/logrus"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/monitoring/v3"
-	"github.com/Sirupsen/logrus"
-	"cloud.google.com/go/datastore"
+	"github.com/GannettDigital/go-newrelic-plugin/helpers"
 )
 
-var metrics = []string{
+var stackdriverEndpoints = []string{
 	"datastore.googleapis.com/api/request_count",
-	"datastore.googleapis.com/entity/read_sizes",
-	"datastore.googleapis.com/entity/write_sizes",
 	"datastore.googleapis.com/index/write_count",
+	//"datastore.googleapis.com/entity/read_sizes",
+	//"datastore.googleapis.com/entity/write_sizes", -- TODO create appropriate sized and number of buckets
 }
 
 type DatastoreKind struct {
@@ -32,34 +33,82 @@ type DatastoreKind struct {
 	Bytes               int       `datastore:"bytes"`
 }
 
+// PluginData defines the format of the output JSON that plugins will return
+type PluginData struct {
+	Name            string                   `json:"name"`
+	ProtocolVersion string                   `json:"protocol_version"`
+	PluginVersion   string                   `json:"plugin_version"`
+	Metrics         []MetricData             `json:"metrics"`
+	Inventory       map[string]InventoryData `json:"inventory"`
+	Events          []EventData              `json:"events"`
+	Status          string                   `json:"status"`
+}
+
+// InventoryData is the data type for inventory data produced by a plugin data
+// source and emitted to the agent's inventory data store
+type InventoryData map[string]interface{}
+
+// MetricData is the data type for events produced by a plugin data source and
+// emitted to the agent's metrics data store
+type MetricData map[string]interface{}
+
+// EventData is the data type for single shot events
+type EventData map[string]interface{}
+
+// NAME - name of plugin
+const NAME string = "datastore"
+
+// PROVIDER -
+const PROVIDER string = "datastore" //we might want to make this an env tied to nginx version or app name maybe...
+
+// ProtocolVersion -
+const ProtocolVersion string = "1"
+
+type StackdriverMetric struct {
+	TimeSeries []struct {
+		Metric struct {
+			Labels struct {
+				ApiMethod    string `json:"api_method"`
+				ResponseCode string `json:"response_code"`
+			} `json:"labels"`
+			Type string `json:"type"`
+		} `json:"metric"`
+		MetricKind string `json:"metricKind"`
+		Points     []struct {
+			Interval struct {
+				EndTime   time.Time `json:"endTime"`
+				StartTime time.Time `json:"startTime"`
+			} `json:"interval"`
+			Value struct {
+				Int64Value int64 `json:"int64Value,string"`
+			} `json:"value"`
+		} `json:"points"`
+		Resource struct {
+			Labels struct {
+				ModuleID  string `json:"module_id"`
+				ProjectID string `json:"project_id"`
+				VersionID string `json:"version_id"`
+			} `json:"labels"`
+			Type string `json:"type"`
+		} `json:"resource"`
+		ValueType string `json:"valueType"`
+	} `json:"timeSeries"`
+}
+
 func Run(log *logrus.Logger, prettyPrint bool, version string) {
-	stackdriverMetrics()
-	datastoreStats()
-}
+	var data = PluginData{
+		Name:            NAME,
+		ProtocolVersion: ProtocolVersion,
+		PluginVersion:   version,
+		Inventory:       make(map[string]InventoryData),
+		Metrics:         make([]MetricData, 0),
+		Events:          make([]EventData, 0),
+	}
 
-func datastoreStats(){
 	ctx := context.Background()
-	dsClient, err := datastore.NewClient(ctx,"gannett-api-services-stage")
-	if err != nil{
-		log.Fatal("Error connecting to datastore")
-	}
+	projectID := "gannett-api-services-stage"
 
-
-	q := datastore.NewQuery("__Stat_Kind__").Order("kind_name")
-	kinds := []*DatastoreKind{}
-	_, err = dsClient.GetAll(ctx,q,&kinds)
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	for _, k := range kinds{
-		fmt.Printf("\nkind %q\t%d entries\t%d bytes\n", k.KindName, k.Count, k.Bytes)
-	}
-}
-
-func stackdriverMetrics(){
-	ctx := context.Background()
-
+	//stackdriver metrics
 	hc, err := google.DefaultClient(ctx, monitoring.MonitoringScope)
 
 	if err != nil {
@@ -72,50 +121,88 @@ func stackdriverMetrics(){
 		log.Fatal(err)
 	}
 
+	//loop through datastore stackdriverEndpoints
+	for _, metric := range stackdriverEndpoints {
 
-	projectID := "gannett-api-services-stage"
+		resp, err := listTimeSeries(s, projectID, metric)
 
-	//loop through datastore metrics
-	for _, metric := range metrics {
-		if err := listMetricDescriptors(s, projectID, metric); err != nil {
+		if err != nil {
 			log.Fatal(err)
 		}
-		if err := listTimeSeries(s, projectID, metric); err != nil {
-			log.Fatal(err)
+
+		var requestCountBody StackdriverMetric
+
+		json.Unmarshal(formatResource(resp), &requestCountBody)
+
+		//fmt.Printf("%s",formatResource(resp))
+		for id := range requestCountBody.TimeSeries {
+			data.Metrics = append(data.Metrics, map[string]interface{}{
+				"event_type":                        "DatastoreSample",
+				"provider":                          PROVIDER,
+				"datastoreStackdriver.apiMethod":    requestCountBody.TimeSeries[id].Metric.Labels.ApiMethod,
+				"datastoreStackdriver.responseCode": requestCountBody.TimeSeries[id].Metric.Labels.ResponseCode,
+				"datastoreStackdriver.metricType":   requestCountBody.TimeSeries[id].Metric.Type,
+				"datastoreStackdriver.metricKind":   requestCountBody.TimeSeries[id].MetricKind,
+				"datastoreStackriver.time":          requestCountBody.TimeSeries[id].Points[0].Interval.EndTime.String(),
+				"datastoreStackdriver.value":        strconv.FormatInt(requestCountBody.TimeSeries[0].Points[0].Value.Int64Value, 10),
+				"datastoreStackdriver.projectId":    requestCountBody.TimeSeries[id].Resource.Labels.ProjectID,
+				"datastoreStackdriver.resourceType": requestCountBody.TimeSeries[id].Resource.Type,
+			})
 		}
 	}
-}
 
-
-func listMetricDescriptors(s *monitoring.Service, projectID string, metric string) error {
-
-	resp, err := s.Projects.MetricDescriptors.List(projectResource(projectID)).
-		Filter(fmt.Sprintf("metric.type=%q", metric)).
-		Do()
+	//datastore statistics
+	dsClient, err := datastore.NewClient(ctx, "gannett-api-services-stage")
 	if err != nil {
-		return fmt.Errorf("Could not list metric descriptors: %v", err)
+		log.Fatal("Error connecting to datastore")
 	}
 
-	log.Printf("listMetricDescriptors %s\n", formatResource(resp))
-	return nil
+	q := datastore.NewQuery("__Stat_Kind__").Order("kind_name")
+
+	kinds := []*DatastoreKind{}
+
+	_, err = dsClient.GetAll(ctx, q, &kinds)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, k := range kinds {
+		fmt.Printf("\nkind %q\t%d entries\t%d bytes\n", k.KindName, k.Count, k.Bytes)
+		data.Metrics = append(data.Metrics, map[string]interface{}{
+			"event_type":              "DatastoreSample",
+			"provider":                PROVIDER,
+			"datastoreQuery.time":     time.Now().UTC(),
+			"datastoreQuery.kindName": k.KindName,
+			"datastoreQuery.count":    k.Count,
+			"datastoreQuery.bytes":    k.Bytes,
+		})
+	}
+
+	fatalIfErr(log, helpers.OutputJSON(data,prettyPrint))
 }
 
-func listTimeSeries(s *monitoring.Service, projectID string, metric string) error {
-	startTime := time.Now().UTC().Add(-time.Hour)
-	endTime := startTime.Add(5 * time.Minute)
+func fatalIfErr(log *logrus.Logger, err error) {
+	if err != nil {
+		log.WithError(err).Fatal("can't continue")
+	}
+}
+
+func listTimeSeries(s *monitoring.Service, projectID string, metric string) (*monitoring.ListTimeSeriesResponse, error) {
+	startTime := time.Now().UTC().Add(time.Minute * -3)
+	endTime := time.Now().UTC()
 
 	resp, err := s.Projects.TimeSeries.List(projectResource(projectID)).
-		PageSize(3).
 		Filter(fmt.Sprintf("metric.type=\"%s\"", metric)).
 		IntervalStartTime(startTime.Format(time.RFC3339)).
 		IntervalEndTime(endTime.Format(time.RFC3339)).
 		Do()
+
 	if err != nil {
-		return fmt.Errorf("Could not list time series: %v", err)
+		return nil, err
 	}
 
-	log.Printf("listTimeseries %s\n", formatResource(resp))
-	return nil
+	return resp, nil
 }
 
 func projectResource(projectID string) string {
