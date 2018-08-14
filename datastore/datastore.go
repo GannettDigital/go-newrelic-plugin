b5c66a3d6e78b7e4d95b916fd8af8ea6a6fb0da1
jstorer <jeremyjstorer@gmail.com>
2018-08-14 10:21:03 -0400

    PR changes

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -17,13 +17,16 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/buger/jsonparser"
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/monitoring/v3"
 	"google.golang.org/api/option"
 )
 
 const (
 	// NAME - name of plugin
 	NAME string = "datastore"
+
 	// ProtocolVersion -
 	ProtocolVersion string = "1"
 )
@@ -61,12 +64,13 @@ type PluginData struct {
 	Status          string                   `json:"status"`
 }
 
+//stores datastoreclient
 type Client struct {
-	Dsc DatastoreInterface
+	Dsc DatastoreClient
 }
 
-//DatastoreInterface is used for testing purposes
-type DatastoreInterface interface {
+//DatastoreClient is used for testing purposes
+type DatastoreClient interface {
 	GetAll(ctx context.Context, q *datastore.Query, dst interface{}) (keys []*datastore.Key, err error)
 }
 
@@ -80,6 +84,38 @@ type MetricData map[string]interface{}
 
 // EventData is the data type for single shot events
 type EventData map[string]interface{}
+
+//StackdriverMetric represents fields for stackdriver returns
+type StackdriverMetric struct {
+	TimeSeries []struct {
+		Metric struct {
+			Labels struct {
+				ApiMethod    string `json:"api_method"`
+				ResponseCode string `json:"response_code"`
+			} `json:"labels"`
+			Type string `json:"type"`
+		} `json:"metric"`
+		MetricKind string `json:"metricKind"`
+		Points     []struct {
+			Interval struct {
+				EndTime   time.Time `json:"endTime"`
+				StartTime time.Time `json:"startTime"`
+			} `json:"interval"`
+			Value struct {
+				Int64Value int64 `json:"int64Value,string"`
+			} `json:"value"`
+		} `json:"points"`
+		Resource struct {
+			Labels struct {
+				ModuleID  string `json:"module_id"`
+				ProjectID string `json:"project_id"`
+				VersionID string `json:"version_id"`
+			} `json:"labels"`
+			Type string `json:"type"`
+		} `json:"resource"`
+		ValueType string `json:"valueType"`
+	} `json:"timeSeries"`
+}
 
 func Run(log *logrus.Logger, prettyPrint bool, version string) {
 	var data = PluginData{
@@ -97,11 +133,11 @@ func Run(log *logrus.Logger, prettyPrint bool, version string) {
 	}
 
 	//add query metrics
-	kinds, err := getDatastoreQueryResult(c.Dsc)
+	kinds, err := DatastoreStatKindQueryResult(c.Dsc)
 	if err != nil {
 		log.Fatal(err)
 	}
-	result := getDatastoreData(kinds,projectId)
+	result := DatastoreData(kinds, projectId)
 
 	for _, metricResult := range result {
 		data.Metrics = append(data.Metrics, metricResult)
@@ -109,12 +145,12 @@ func Run(log *logrus.Logger, prettyPrint bool, version string) {
 
 	//add stackdriver metrics
 	for _, metric := range stackdriverEndpoints {
-		resp, err := stackdriverResp(projectId, metric)
+		resp, err := StackdriverResp(projectId, metric)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		result, err := stackdriverData(resp)
+		result, err := StackdriverData(resp)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -126,7 +162,20 @@ func Run(log *logrus.Logger, prettyPrint bool, version string) {
 	fatalIfErr(log, helpers.OutputJSON(data, prettyPrint))
 }
 
-func getDatastoreQueryResult(ds DatastoreInterface) ([]DatastoreKind, error) {
+//NewClient() creates a client for datastore, it primarily exists for testing purposes
+func NewClient() (Client, string, error) {
+	dsClient, projectId, err := ConnectDatastore(base64Config)
+	if err != nil {
+		return Client{}, "", err
+	}
+
+	return Client{
+		dsClient,
+	}, projectId, nil
+}
+
+//DatastoreStatKindQueryResult performs a Query against the datastore and return values using __Stat_Kind__
+func DatastoreStatKindQueryResult(ds DatastoreClient) ([]DatastoreKind, error) {
 	q := datastore.NewQuery("__Stat_Kind__").Order("kind_name")
 
 	var kinds []DatastoreKind
@@ -141,7 +190,8 @@ func getDatastoreQueryResult(ds DatastoreInterface) ([]DatastoreKind, error) {
 	return kinds, nil
 }
 
-func getDatastoreData(kinds []DatastoreKind, projectID string) []map[string]interface{} {
+//DatastoreData converts a []DatastoreKind to a []map[string]interface{} to be used for the final output to new relic
+func DatastoreData(kinds []DatastoreKind, projectID string) []map[string]interface{} {
 	var datastoreData []map[string]interface{}
 	for _, k := range kinds {
 		datastoreData = append(datastoreData, map[string]interface{}{
@@ -162,21 +212,102 @@ func getDatastoreData(kinds []DatastoreKind, projectID string) []map[string]inte
 	return datastoreData
 }
 
-func NewClient() (Client,string, error) {
-	dsClient,projectId,err:=ConnectDatastore(base64Config)
-	if err != nil {
-		return Client{},"", err
-	}
-
-	return Client{
-		dsClient,
-	},projectId, nil
-}
-
 func fatalIfErr(log *logrus.Logger, err error) {
 	if err != nil {
 		log.WithError(err).Fatal("can't continue")
 	}
+}
+
+// ConnectDatastore establishes a datastore.Client from a base64 encoding JSON credentials file.
+func ConnectDatastore(base64Config string) (*datastore.Client, string, error) {
+	jsonConfig, err := base64.StdEncoding.DecodeString(base64Config)
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to decode datastore credentials: %v", err)
+	}
+	projectId, err := jsonparser.GetString(jsonConfig, "project_id")
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to determine project_id from credentials file: %v", err)
+	}
+
+	ctx := context.Background()
+	creds, err := google.CredentialsFromJSON(ctx, jsonConfig, datastore.ScopeDatastore)
+	if err != nil {
+		return nil, "", err
+	}
+	c, err := datastore.NewClient(ctx, projectId, option.WithCredentials(creds))
+	if err != nil {
+		return nil, "", err
+	}
+	return c, projectId, err
+}
+
+//StackdriverResp gets the data of the wanted metric from the stackdriver API. Start time is set at -3 minutes to act as a Timestamp
+//as data from stackdriver is always 3 minutes old and refreshed every 1 minute. This timing also ensures we only ever get 1 point
+//back at a time.
+func StackdriverResp(projectId string, metric string) (*monitoring.ListTimeSeriesResponse, error) {
+	ctx := context.Background()
+	jsonConfig, err := base64.StdEncoding.DecodeString(base64Config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode datastore credentials: %v", err)
+	}
+
+	projectId, err = jsonparser.GetString(jsonConfig, "project_id")
+	if err != nil {
+		return nil, fmt.Errorf("failed to determine project_id from credentials file: %v", err)
+	}
+
+	creds, err := google.CredentialsFromJSON(ctx, jsonConfig, monitoring.MonitoringScope)
+
+	hc := oauth2.NewClient(ctx, creds.TokenSource)
+
+	s, err := monitoring.New(hc)
+	if err != nil {
+		return nil, err
+	}
+
+	startTime := time.Now().UTC().Add(time.Minute * -3)
+	endTime := time.Now().UTC()
+
+	resp, err := s.Projects.TimeSeries.List(projectResource(projectId)).
+		Filter(fmt.Sprintf("metric.type=\"%s\"", metric)).
+		IntervalStartTime(startTime.Format(time.RFC3339)).
+		IntervalEndTime(endTime.Format(time.RFC3339)).
+		Do()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+
+}
+
+//StackdriverData converts a ListTimeSeriesResponse to a []map[string]interface{} to be used for the final output to be sent to new relic
+func StackdriverData(resp *monitoring.ListTimeSeriesResponse) ([]map[string]interface{}, error) {
+	var stackdriverMetricBody StackdriverMetric
+	var metricResult []map[string]interface{}
+
+	err := json.Unmarshal(formatResource(resp), &stackdriverMetricBody)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, item := range stackdriverMetricBody.TimeSeries {
+		metricResult = append(metricResult, map[string]interface{}{
+			"event_type":                        "DatastoreSample",
+			"provider":                          "datastoreStackdriver",
+			"datastoreStackdriver.apiMethod":    item.Metric.Labels.ApiMethod,
+			"datastoreStackdriver.responseCode": item.Metric.Labels.ResponseCode,
+			"datastoreStackdriver.metricType":   item.Metric.Type,
+			"datastoreStackdriver.metricKind":   item.MetricKind,
+			"datastoreStackdriver.timestamp":    item.Points[0].Interval.StartTime.Unix(),
+			"datastoreStackdriver.value":        item.Points[0].Value.Int64Value,
+			"datastoreStackdriver.projectId":    item.Resource.Labels.ProjectID,
+			"datastoreStackdriver.resourceType": item.Resource.Type,
+		})
+	}
+
+	return metricResult, nil
 }
 
 func projectResource(projectID string) string {
@@ -190,27 +321,4 @@ func formatResource(resource interface{}) []byte {
 		panic(err)
 	}
 	return b
-}
-
-// ConnectDatastore establishes a datastore.Client from a base64 encoding JSON credentials file.
-func ConnectDatastore(base64Config string) (*datastore.Client,string, error) {
-	jsonConfig, err := base64.StdEncoding.DecodeString(base64Config)
-	if err != nil {
-		return nil,"", fmt.Errorf("failed to decode datastore credentials: %v", err)
-	}
-	projectId, err := jsonparser.GetString(jsonConfig, "project_id")
-	if err != nil {
-		return nil,"", fmt.Errorf("failed to determine project_id from credentials file: %v", err)
-	}
-
-	ctx := context.Background()
-	creds, err := google.CredentialsFromJSON(ctx, jsonConfig, datastore.ScopeDatastore)
-	if err != nil {
-		return nil,"", err
-	}
-	c,err:=datastore.NewClient(ctx, projectId, option.WithCredentials(creds))
-	if err != nil {
-		return nil,"", err
-	}
-	return c,projectId,err
 }
--- datastore/datastore_test.go
+++ datastore/datastore_test.go
@@ -8,6 +8,8 @@ import (
 
 	"cloud.google.com/go/datastore"
 	"github.com/franela/goblin"
+	"google.golang.org/api/monitoring/v3"
+	"google.golang.org/api/googleapi"
 )
 
 type FakeDataStoreClient struct {
@@ -47,7 +49,7 @@ func (fdsc FakeDataStoreClient) GetAll(ctx context.Context, q *datastore.Query, 
 	return []*datastore.Key{}, err
 }
 
-func TestGetDatastoreQueryResult(t *testing.T) {
+func TestDatastoreStatKindQueryResult(t *testing.T) {
 	g := goblin.Goblin(t)
 	var tests = []struct {
 		datastoreKind []DatastoreKind
@@ -105,10 +107,10 @@ func TestGetDatastoreQueryResult(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		g.Describe("getDatastoreQueryResult()", func() {
+		g.Describe("DatastoreStatKindQueryResult()", func() {
 			g.It(test.description, func() {
 				fakeClient := NewFakeClient(test.datastoreKind, test.err)
-				kindsResult, err := getDatastoreQueryResult(fakeClient.Dsc)
+				kindsResult, err := DatastoreStatKindQueryResult(fakeClient.Dsc)
 				g.Assert(err).Equal(test.expectedErr)
 				g.Assert(kindsResult).Equal(test.datastoreKind)
 			})
@@ -117,7 +119,7 @@ func TestGetDatastoreQueryResult(t *testing.T) {
 	}
 }
 
-func TestGetDataStoreData(t *testing.T) {
+func TestDataStoreData(t *testing.T) {
 	g := goblin.Goblin(t)
 	var tests = []struct {
 		projectID           string
@@ -218,9 +220,9 @@ func TestGetDataStoreData(t *testing.T) {
 		},
 	}
 	for _, test := range tests {
-		g.Describe("getDatastoreData()", func() {
+		g.Describe("DatastoreData()", func() {
 			g.It(test.description, func() {
-				data := getDatastoreData(test.datastoreKind, test.projectID)
+				data := DatastoreData(test.datastoreKind, test.projectID)
 				g.Assert(data).Equal(test.datastoreDataWanted)
 			})
 		})
@@ -228,4 +230,178 @@ func TestGetDataStoreData(t *testing.T) {
 	}
 }
 
+func TestStackdriverData(t *testing.T) {
+	g := goblin.Goblin(t)
+	var tests = []struct {
+		resp        monitoring.ListTimeSeriesResponse
+		dataWanted  []map[string]interface{}
+		errWanted   error
+		description string
+	}{
+		{
+			monitoring.ListTimeSeriesResponse{
+				ExecutionErrors: []*monitoring.Status{},
+				NextPageToken:   "",
+				TimeSeries: []*monitoring.TimeSeries{
+					{
+						Metric: &monitoring.Metric{
+							Labels: map[string]string{
+								"api_method":    "Commit",
+								"response_code": "OK",
+							},
+							Type: "datastore.googleapis.com/api/request_count",
+						},
+						MetricKind: "DELTA",
+						Points: []*monitoring.Point{
+							{
+								Interval: &monitoring.TimeInterval{
+									EndTime:   "2018-08-10T20:17:37.754Z",
+									StartTime: "2018-08-10T20:16:37.754Z",
+								},
+								Value: &monitoring.TypedValue{
+									Int64Value: googleapi.Int64(2),
+								},
+							},
+						},
+						Resource: &monitoring.MonitoredResource{
+							Labels: map[string]string{
+								"module_id":  "__unknown__",
+								"project_id": "gannett-api-services-stage",
+								"version_id": "__unknown__",
+							},
+							Type: "datastore_request",
+						},
+						ValueType: "INT64",
+					},
+				},
+			},
+			[]map[string]interface{}{
+				{
+					"event_type":                        "DatastoreSample",
+					"provider":                          "datastoreStackdriver",
+					"datastoreStackdriver.apiMethod":    "Commit",
+					"datastoreStackdriver.responseCode": "OK",
+					"datastoreStackdriver.metricType":   "datastore.googleapis.com/api/request_count",
+					"datastoreStackdriver.metricKind":   "DELTA",
+					"datastoreStackdriver.timestamp":    int64(1533932197),
+					"datastoreStackdriver.value":        int64(2),
+					"datastoreStackdriver.projectId":    "gannett-api-services-stage",
+					"datastoreStackdriver.resourceType": "datastore_request",
+				},
+			},
+			nil,
+			"One timeseries point",
+		},
+		{
+			monitoring.ListTimeSeriesResponse{
+				ExecutionErrors: []*monitoring.Status{},
+				NextPageToken:   "",
+				TimeSeries: []*monitoring.TimeSeries{
+					{
+						Metric: &monitoring.Metric{
+							Labels: map[string]string{
+								"api_method":    "Commit",
+								"response_code": "OK",
+							},
+							Type: "datastore.googleapis.com/api/request_count",
+						},
+						MetricKind: "DELTA",
+						Points: []*monitoring.Point{
+							{
+								Interval: &monitoring.TimeInterval{
+									EndTime:   "2018-08-10T20:17:37.754Z",
+									StartTime: "2018-08-10T20:16:37.754Z",
+								},
+								Value: &monitoring.TypedValue{
+									Int64Value: googleapi.Int64(2),
+								},
+							},
+						},
+						Resource: &monitoring.MonitoredResource{
+							Labels: map[string]string{
+								"module_id":  "__unknown__",
+								"project_id": "gannett-api-services-stage",
+								"version_id": "__unknown__",
+							},
+							Type: "datastore_request",
+						},
+						ValueType: "INT64",
+					},
+					{
+						Metric: &monitoring.Metric{
+							Labels: map[string]string{
+								"api_method":    "Query",
+								"response_code": "OK",
+							},
+							Type: "datastore.googleapis.com/api/request_count",
+						},
+						MetricKind: "DELTA",
+						Points: []*monitoring.Point{
+							{
+								Interval: &monitoring.TimeInterval{
+									EndTime:   "2018-08-10T20:17:37.754Z",
+									StartTime: "2018-08-10T20:16:37.754Z",
+								},
+								Value: &monitoring.TypedValue{
+									Int64Value: googleapi.Int64(2),
+								},
+							},
+						},
+						Resource: &monitoring.MonitoredResource{
+							Labels: map[string]string{
+								"module_id":  "__unknown__",
+								"project_id": "gannett-api-services-stage",
+								"version_id": "__unknown__",
+							},
+							Type: "datastore_request",
+						},
+						ValueType: "INT64",
+					},
+				},
+			},
+			[]map[string]interface{}{
+				{
+					"event_type":                        "DatastoreSample",
+					"provider":                          "datastoreStackdriver",
+					"datastoreStackdriver.apiMethod":    "Commit",
+					"datastoreStackdriver.responseCode": "OK",
+					"datastoreStackdriver.metricType":   "datastore.googleapis.com/api/request_count",
+					"datastoreStackdriver.metricKind":   "DELTA",
+					"datastoreStackdriver.timestamp":    int64(1533932197),
+					"datastoreStackdriver.value":        int64(2),
+					"datastoreStackdriver.projectId":    "gannett-api-services-stage",
+					"datastoreStackdriver.resourceType": "datastore_request",
+				},
+				{
+					"event_type":                        "DatastoreSample",
+					"provider":                          "datastoreStackdriver",
+					"datastoreStackdriver.apiMethod":    "Query",
+					"datastoreStackdriver.responseCode": "OK",
+					"datastoreStackdriver.metricType":   "datastore.googleapis.com/api/request_count",
+					"datastoreStackdriver.metricKind":   "DELTA",
+					"datastoreStackdriver.timestamp":    int64(1533932197),
+					"datastoreStackdriver.value":        int64(2),
+					"datastoreStackdriver.projectId":    "gannett-api-services-stage",
+					"datastoreStackdriver.resourceType": "datastore_request",
+				},
+			},
+			nil,
+			"Two timeseries points",
+		},
+	}
 
+	for _, test := range tests {
+		g.Describe("StackdriverData()", func() {
+			g.It(test.description, func() {
+				data, err := StackdriverData(&test.resp)
+				for id, _ := range data {
+					g.Assert(data[id]).Equal(test.dataWanted[id])
+
+				}
+				g.Assert(err).Equal(test.errWanted)
+			})
+		})
+
+	}
+
+}
